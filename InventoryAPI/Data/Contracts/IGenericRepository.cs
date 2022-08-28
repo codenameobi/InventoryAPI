c7e217e8adb153afdd78acac35ff0af778a955db
admin <tobynwude@gmail.com>
2022-08-28 21:41:30 +0100

    added generic repository 1

---
+++ InventoryAPI/Data/Contracts/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using InventoryAPI.Models;
+
+namespace InventoryAPI.Data.Contracts
+{
+	public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+	{
+		Task<TEntity> GetAsync(int? id);
+        Task<List<TEntity>> GetAllAsync();
+		Task<TEntity> AddAsync(TEntity entity);
+		Task DeleteAsync(int id);
+        Task UpdateAsync(TEntity entity);
+		Task<bool> Exists(int id);
+    }
+
+}
+
